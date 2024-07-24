939b8e6c0f75f8d362448e77c8f77b75a7afe3a0
south <fancustard9@gmail.com>
2024-07-24 18:46:15 +0900

    chapter4-3(p106)まで

---
+++ app/members/page.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+const data = {
+  contents: [
+    {
+      id: "1",
+      image: {
+        url: "/member1.png",
+        width: 240,
+        height: 240,
+      },
+      name: "にこにこ",
+      position: "CEO",
+      profile: "いつもにこにこしていて優しい",
+    },
+    {
+      id: "2",
+      image: {
+        url: "/member2.png",
+        width: 240,
+        height: 240,
+      },
+      name: "はいから",
+      position: "COO",
+      profile: "いつもおしゃれで、時代を先駆けていく",
+    },
+    {
+      id: "3",
+      image: {
+        url: "/member3.png",
+        width: 240,
+        height: 240,
+      },
+      name: "スミス・テリー",
+      position: "CTO",
+      profile: "「常に思考」がモットーの仕事のできる超人",
+    },
+  ],
+};
+
+export default function Page() {
+  return (
+    // <div style={{ margin: 20 }}>
+    //   <h1>メンバーページ</h1>
+    // </div>
+
+    <div className={styles.container}>
+      {data.contents.length === 0 ? (
+        <p className={styles.empty}>メンバーが登録されていません</p>
+      ) : (
+        <ul>
+          {data.contents.map((member) => (
+            <li key={member.id} className={styles.list}>
+              <Image
+                src={member.image.url}
+                alt=""
+                width={member.image.width}
+                height={member.image.height}
+                className={styles.image}
+              />
+              <dl>
+                <dt className={styles.name}>{member.name}</dt>
+                <dd className={styles.position}>{member.position}</dd>
+                <dd className={styles.profile}>{member.profile}</dd>
+              </dl>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
