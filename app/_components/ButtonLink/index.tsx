db5466808d6e7254f36cb63e5aaaf1b37817b420
south <fancustard9@gmail.com>
2024-07-24 16:35:20 +0900

    headerの見た目を直した

--- app/_components/ButtonLink/index.tsx
+++ app/_components/ButtonLink/index.tsx
@@ -1,9 +1,8 @@
-import { ReactNode } from "react";
 import styles from "./index.module.css";
 
 type Props = {
   href: string;
-  children: ReactNode;
+  children: React.ReactNode;
 };
 
 export default function Button({ href, children }: Props) {
