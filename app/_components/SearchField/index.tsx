e58f5b0737312ec95400effe861403f1d96ae36b
south <fancustard9@gmail.com>
2024-07-29 16:56:20 +0900

    chapter7(p212)まで

--- app/_components/SearchField/index.tsx
+++ app/_components/SearchField/index.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
-import styles from "./inex.module.css";
+"use client";
 
-export default function SearchField() {
+import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
+import styles from "./inex.module.css";
+import { Suspense } from "react";
+
+// export default function SearchField() {
+function SearchFieldComPonent() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const q = e.currentTarget.elements.namedItem("q");
+    if (q instanceof HTMLInputElement) {
+      const params = new URLSearchParams();
+      params.set("q", q.value.trim());
+      router.push(`/news/search?${params.toString()}`);
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <label className={styles.search}>
         <Image
           src="/search.png"
@@ -15,10 +33,19 @@ export default function SearchField() {
         <input
           type="text"
           name="q"
+          defaultValue={searchParams.get("q") ?? undefined}
           placeholder="キーワードを入力"
           className={styles.searchInput}
         />
       </label>
     </form>
+  );
+}
+
+export default function SearchField() {
+  return (
+    <Suspense>
+      <SearchFieldComPonent />
+    </Suspense>
   );
 }
